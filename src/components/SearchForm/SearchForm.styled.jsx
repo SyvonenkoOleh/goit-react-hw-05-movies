4d85b625a1b447d31f60edd7262e5a0d639a2d2a
SyvonenkoOleh <115041334+SyvonenkoOleh@users.noreply.github.com>
2023-06-01 08:09:27 +0300

    add

--- src/components/SearchForm/SearchForm.styled.jsx
+++ src/components/SearchForm/SearchForm.styled.jsx
@@ -13,7 +13,6 @@ export const Input = styled.input`
   padding: 12px 16px;
   margin-right: 36px;
   border-radius: 12px;
-  border: 1px solid black;
   width: 30vw;
   border: 1px solid rgba(0, 128, 75, 0.7);
   box-shadow: 0px 12px 8px 4px #cab1b14d;
